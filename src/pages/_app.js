fa9b8f8d755c4f6ece594bbecf1f419ac7b4afdf
sangkanfaiq <sangkanfaiq@gmail.com>
2022-08-05 16:04:14 +0700

    feat: add signin page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,12 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
+import "bootstrap/dist/css/bootstrap.css";
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+
   return <Component {...pageProps} />
 }
 
