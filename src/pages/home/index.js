228e90e923c74c1dd623371767fe18ec286a4f02
zainulmbrk <zainulmbrk.id@gmail.com>
2022-08-06 23:32:23 +0800

    feat: add ssr in home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,8 +1,14 @@
-import Homepage from "../../layouts/home";
+import Homepage from "../../layouts/home"
 
-const Home = ()=>{
+const Home = ({data})=>{
     return(<>
-        <Homepage/>
+        <Homepage data={data} />
     </>)
 }
 export default Home
+
+export async function getServerSideProps(){
+    const res = await fetch(`http://localhost:1102/api/v1/product/`)
+    const data = await res.json()
+  return { props: { data } }
+}
