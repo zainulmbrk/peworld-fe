d8b29c47139403179d14f3329b75348a1cbcd192
sangkanfaiq <sangkanfaiq@gmail.com>
2022-08-08 12:13:36 +0700

    feat: add experience on profile

--- src/layouts/EmployeeProfile/index.js
+++ src/layouts/EmployeeProfile/index.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Footer from "../../components/footer";
 import Navbar from "../../components/navbar/navbar";
 import ProfileDetailsLayout from "./components/ProfileDetails";
 import styles from "./Employee.module.scss";
 
 const EmployeeProfileLayout = () => {
+  const [toggle, setToggle] = useState(true);
   return (
     <>
-    <Navbar />
+      <Navbar />
       <div className={styles.profileLayout}>
         <div className="container">
           <div className="row">
@@ -15,43 +16,76 @@ const EmployeeProfileLayout = () => {
               <ProfileDetailsLayout />
             </div>
             <div className="col-md-8">
-                <div className={styles.portofolio}>
-                    <div className={styles.portoTitle}>
-                        <h3 className={styles.active}>Portofolio</h3>
-                        <h3>Pengalaman kerja</h3>
-                    </div>
-                    <div className={styles.listPorto}>
-                        <div className={styles.portoCard}>
-                            <img src="/portofolio/porto6.png" alt="portofolio" />
-                            <p>Reminder App</p>
-                        </div>
-                        <div className={styles.portoCard}>
-                            <img src="/portofolio/porto1.png" alt="portofolio" />
-                            <p>Social Media App</p>
-                        </div>
-                        <div className={styles.portoCard}>
-                            <img src="/portofolio/porto2.png" alt="portofolio" />
-                            <p>Project management web</p>
-                        </div>
-                        <div className={styles.portoCard}>
-                            <img src="/portofolio/porto3.png" alt="portofolio" />
-                            <p>Reminder App</p>
-                        </div>
-                        <div className={styles.portoCard}>
-                            <img src="/portofolio/porto4.png" alt="portofolio" />
-                            <p>Social Media App</p>
-                        </div>
-                        <div className={styles.portoCard}>
-                            <img src="/portofolio/porto5.png" alt="portofolio" />
-                            <p>Project management web</p>
-                        </div>
-                    </div>
+              <div className={styles.portofolio}>
+                <div className={styles.portoTitle}>
+                  <h3 className={toggle ? `${styles.active}` : ""} onClick={() => setToggle(true)}>Portofolio</h3>
+                  <h3 className={toggle ? "" :`${styles.active}`} onClick={() => setToggle(false)}>Pengalaman kerja</h3>
                 </div>
+                {toggle ? (
+                  <div className={styles.listPorto}>
+                    <div className={styles.portoCard}>
+                      <img src="/portofolio/porto6.png" alt="portofolio" />
+                      <p>Reminder App</p>
+                    </div>
+                    <div className={styles.portoCard}>
+                      <img src="/portofolio/porto1.png" alt="portofolio" />
+                      <p>Social Media App</p>
+                    </div>
+                    <div className={styles.portoCard}>
+                      <img src="/portofolio/porto2.png" alt="portofolio" />
+                      <p>Project management web</p>
+                    </div>
+                    <div className={styles.portoCard}>
+                      <img src="/portofolio/porto3.png" alt="portofolio" />
+                      <p>Reminder App</p>
+                    </div>
+                    <div className={styles.portoCard}>
+                      <img src="/portofolio/porto4.png" alt="portofolio" />
+                      <p>Social Media App</p>
+                    </div>
+                    <div className={styles.portoCard}>
+                      <img src="/portofolio/porto5.png" alt="portofolio" />
+                      <p>Project management web</p>
+                    </div>
+                  </div>
+                ) : (
+                  <>
+                    <div className={styles.experience}>
+                      <div className={styles.expDetails}>
+                        <div className={styles.expImage}>
+                          <img src="/images/img8.png" alt="experience" />
+                        </div>
+                        <div className={styles.expInfo}>
+                          <h3>Engineer</h3>
+                          <h4>Tokopedia</h4>
+                          <h5>
+                            July 2019 - January 2020 - <span>6 months</span>
+                          </h5>
+                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum erat orci, mollis nec gravida sed, ornare quis urna. Curabitur eu lacus fringilla, vestibulum risus at.</p>
+                        </div>
+                      </div>
+                      <div className={styles.expDetails}>
+                        <div className={styles.expImage}>
+                          <img src="/images/img8.png" alt="experience" />
+                        </div>
+                        <div className={styles.expInfo}>
+                          <h3>Web Developer</h3>
+                          <h4>Tokopedia</h4>
+                          <h5>
+                            July 2019 - January 2020 - <span>6 months</span>
+                          </h5>
+                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum erat orci, mollis nec gravida sed, ornare quis urna. Curabitur eu lacus fringilla, vestibulum risus at.</p>
+                        </div>
+                      </div>
+                    </div>
+                  </>
+                )}
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </>
   );
 };
