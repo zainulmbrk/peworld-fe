69bc3d25228f1486c1622f430c768c25296a1815
sangkanfaiq <sangkanfaiq@gmail.com>
2022-09-05 22:26:04 +0700

    feat: add animate on scroll

--- src/layouts/LandingPage/components/WhyUs/index.js
+++ src/layouts/LandingPage/components/WhyUs/index.js
@@ -5,27 +5,27 @@ const WhyUs = () => {
   return (
     <div className="container">
         <div className={`${styles.whyUs} row`}>
-            <div className={`${styles.left} col-md-6`}>
+            <div className={`${styles.left} col-md-6`} data-aos="fade-right" data-aos-delay="900">
                 <img src="images/img1.png" alt="logo" />
             </div>
-            <div className={`${styles.right} col-md-6`}>
+            <div className={`${styles.right} col-md-6`} data-aos="fade-left" data-aos-delay="1100">
                 <h1>Kenapa harus mencari talent di Peworld?</h1>
                 <div className={styles.detailsBox}>
                     <div className={styles.details}>
                         <span><img src="/icons/check.svg" alt="check" /></span>
-                        Lorem ipsum dolor sit amet.
+                        Talent berbakat dari berbagai bidang
                     </div>
                     <div className={styles.details}>
                         <span><img src="/icons/check.svg" alt="check" /></span>
-                        Lorem ipsum dolor sit amet.
+                        Siap tempur secara professional
                     </div>
                     <div className={styles.details}>
                         <span><img src="/icons/check.svg" alt="check" /></span>
-                        Lorem ipsum dolor sit amet.
+                       Talent menguasai bahasa pemrograman terkini
                     </div>
                     <div className={styles.details}>
                         <span><img src="/icons/check.svg" alt="check" /></span>
-                        Lorem ipsum dolor sit amet.
+                       Terdiri dari perusahaan ternama
                     </div>
                 </div>
             </div>
--- src/layouts/hire/index.js
+++ src/layouts/hire/index.js
@@ -1,11 +1,12 @@
 import Footer from "../../components/footer";
 import Navbar from "../../components/navbar/navbar";
+import NavbarLogin from "../../components/navbar/navbarLogin";
 import Hiring from "./components/hiring";
 
 const HiringPeople = ({ data }) => {
   return (
     <>
-      <Navbar />
+      {profile_id ? <NavbarLogin/> : <Navbar /> }
       <Hiring data={data} />
       <Footer />
     </>
